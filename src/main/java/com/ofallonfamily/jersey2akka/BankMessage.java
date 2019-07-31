4453a02c0cf8c6332e76f2a80582246b88836ad6
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-07-31 19:23:01 +0530

    Added the required methods

--- src/main/java/com/ofallonfamily/jersey2akka/BankMessage.java
+++ src/main/java/com/ofallonfamily/jersey2akka/BankMessage.java
@@ -1,6 +1,24 @@
 package com.ofallonfamily.jersey2akka;
 
 public class BankMessage {
-
+    private String MessageType;
+    private String Message;
+    public String getMessageType() {
+        return MessageType;
+    }
+    public void setMessageType(String messageType) {
+        MessageType = messageType;
+    }
+    public String getMessage() {
+        return Message;
+    }
+    public void setMessage(String message) {
+        Message = message;
+    }
+    public BankMessage(String messageType, String message) {
+        super();
+        MessageType = messageType;
+        Message = message;
+    }
 	
 }
