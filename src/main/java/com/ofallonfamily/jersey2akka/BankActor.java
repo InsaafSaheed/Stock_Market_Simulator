30e0775191dccb706e4cd8c7e3758b05380d5095
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-07-31 19:23:54 +0530

    Added the required methods and variables

--- src/main/java/com/ofallonfamily/jersey2akka/BankActor.java
+++ src/main/java/com/ofallonfamily/jersey2akka/BankActor.java
@@ -9,6 +9,28 @@ import akka.event.LoggingAdapter;
 
 public class BankActor extends UntypedActor{
 
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    public static Props mkProps() {
+        return Props.create(BankActor.class);
+    }
+    @Override
+    public void preStart() {
+        log.debug("starting");
+    }
 
+    @Override
+    public void onReceive(Object message) throws Exception {
+        BankMessage msg=(BankMessage)message;
+        if(msg instanceof BankMessage) {
+            if(msg.getMessageType().contentEquals("bank-details")) {
+                getSender().tell(BankAccountDAO.get(msg.getMessage()), getSelf());
+            }else if(msg.getMessageType().contentEquals("account-transactions")) {
+                getSender().tell(MoneyTransactionDAO.getTransactions(msg.getMessage()), getSelf());
+            }else if(msg.getMessageType().contentEquals("bank-accounts")) {
+                getSender().tell(BankAccountDAO.getAll(), getSelf());
+            }
+        }
+
+    }
 
 }
