852b24ed6474e7e36e94bcbf5bfd2000a72a4c82
Lahiru samarasinghe <ldilshan782@gmail.com>
2019-08-01 07:01:26 +0530

    initial commit

---
+++ src/main/java/ae/stock/entities/AnalystSuggession.java
@@ -0,0 +1,13 @@
+package ae.stock.entities;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlElement; 
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "recommendation") 
+public class AnalystSuggession implements Serializable {
+
+	
+	
+}
