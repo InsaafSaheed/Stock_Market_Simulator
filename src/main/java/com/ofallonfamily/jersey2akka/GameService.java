a76db0ff0569159c520f932e18fcf79fb2f45e9c
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-08-01 15:55:03 +0530

    Updated Game Service class

--- src/main/java/com/ofallonfamily/jersey2akka/GameService.java
+++ src/main/java/com/ofallonfamily/jersey2akka/GameService.java
@@ -25,5 +25,284 @@ import scala.concurrent.duration.Duration;
 
 @Path("/game")
 public class GameService {
+    @Context ActorSystem actorSystem;
+    LoggingAdapter log;
 
+    @GET
+    @Path("init")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void initMarket(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/marketRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new MarketMessage("init",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("players")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getPlayers(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/marketRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new MarketMessage("players",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("companies")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getCompanies(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/marketRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new MarketMessage("companies",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("current-round")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getRound(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/clockRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new ClockMessage("current-round",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("new-round")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void newRound(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/clockRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new ClockMessage("new-round",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("player-stock/{player_name}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getPlayerStocks(@PathParam("player_name") String player_name,@Suspended final AsyncResponse res) {
+        ActorSelection playerActor = actorSystem.actorSelection("/user/playerRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(playerActor, new PlayerMessage("player-shares",player_name), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("bank-accounts")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getAccounts(@Suspended final AsyncResponse res) {
+        ActorSelection bankActor = actorSystem.actorSelection("/user/bankRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(bankActor, new BankMessage("bank-accounts",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+
+    @GET
+    @Path("ai-players")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getAIs(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/marketRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new MarketMessage("ai-players",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+    @GET
+    @Path("company-trends")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void getCompanyTrends(@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/marketRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new MarketMessage("company-trends",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
+    @GET
+    @Path("update_round/{player_name}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @ManagedAsync
+    public void updateRound(@PathParam("player_name") String player_name,@Suspended final AsyncResponse res) {
+        ActorSelection marketActor = actorSystem.actorSelection("/user/clockRouter");
+        Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new ClockMessage("update-round",player_name), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+
+        },actorSystem.dispatcher());
+    }
 }
