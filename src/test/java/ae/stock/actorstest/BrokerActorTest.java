4dd396516b48a6ade3d5a4593840a9533374d399
Shanuka Rathnayake <shanukaratnayake@gmail.com>
2019-08-01 22:48:52 +0530

    Added the required methods and variables

---
+++ src/test/java/ae/stock/actorstest/BrokerActorTest.java
@@ -0,0 +1,20 @@
+package ae.stock.actorstest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.ofallonfamily.jersey2akka.AfterEffectsApplication;
+
+public class BrokerActorTest extends JerseyTest{
+	
+
+
+}
