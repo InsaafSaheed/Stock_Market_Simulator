e3795492fbf23282872aca67eb95379e07428c0c
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-08-01 08:50:26 +0530

    Updated Clock Actor and Message classes

--- src/main/java/com/ofallonfamily/jersey2akka/ClockActor.java
+++ src/main/java/com/ofallonfamily/jersey2akka/ClockActor.java
@@ -10,6 +10,39 @@ import akka.event.LoggingAdapter;
 
 public class ClockActor extends UntypedActor{
 
+    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    public static Props mkProps() {
+        return Props.create(ClockActor.class);
+    }
+    @Override
+    public void preStart() {
+        log.debug("starting");
+    }
+    @Override
+    public void onReceive(Object message) throws Exception {
+        ClockMessage msg=(ClockMessage)message;
+        if(msg instanceof ClockMessage) {
+            if(msg.getMessageType().equals("current-round")) {
+                getSender().tell(GameCore.getCurrent_round(), getSelf());
+            }else if(msg.getMessageType().equals("new-round")) {
+                GameCore.setCurrent_round(GameCore.getCurrent_round()+1);
+                getSender().tell(GameCore.getCurrent_round(), getSelf());
+            }else if(msg.getMessageType().equals("update-round")) {
+                Player p=new Player();
+                for(Player plyr:PlayerDAO.getAll()) {
+                    if(plyr.getPlayer_name().equals(msg.getMessage())) {
+                        p=plyr;
+                        int cur_round=p.getCurrent_round();
+                        if(cur_round<10) {
+                            p.setCurrent_round(p.getCurrent_round()+1);
+                            PlayerDAO.update(p);
+                        }
 
+                    }
+                }
+                getSender().tell(p, getSelf());
+            }
+        }
+    }
 
 }
--- src/main/java/com/ofallonfamily/jersey2akka/ClockMessage.java
+++ src/main/java/com/ofallonfamily/jersey2akka/ClockMessage.java
@@ -1,6 +1,24 @@
 package com.ofallonfamily.jersey2akka;
 
 public class ClockMessage {
-
+    private String MessageType;
+    private String Message;
+    public String getMessageType() {
+        return MessageType;
+    }
+    public void setMessageType(String messageType) {
+        MessageType = messageType;
+    }
+    public String getMessage() {
+        return Message;
+    }
+    public void setMessage(String message) {
+        Message = message;
+    }
+    public ClockMessage(String messageType, String message) {
+        super();
+        MessageType = messageType;
+        Message = message;
+    }
 	
 }
