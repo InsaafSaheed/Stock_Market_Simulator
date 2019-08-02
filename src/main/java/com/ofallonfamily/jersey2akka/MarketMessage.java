5210955dda8ad8caf94ab4b6775ed5cda870a722
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-08-02 08:32:12 +0000

    Updated Market Message

--- src/main/java/com/ofallonfamily/jersey2akka/MarketMessage.java
+++ src/main/java/com/ofallonfamily/jersey2akka/MarketMessage.java
@@ -2,5 +2,23 @@ package com.ofallonfamily.jersey2akka;
 
 public class MarketMessage {
 
-	
+	private String MessageType;
+	private String Message;
+	public String getMessageType() {
+		return MessageType;
+	}
+	public void setMessageType(String messageType) {
+		MessageType = messageType;
+	}
+	public String getMessage() {
+		return Message;
+	}
+	public void setMessage(String message) {
+		Message = message;
+	}
+	public MarketMessage(String messageType, String message) {
+		super();
+		MessageType = messageType;
+		Message = message;
+	}
 }
