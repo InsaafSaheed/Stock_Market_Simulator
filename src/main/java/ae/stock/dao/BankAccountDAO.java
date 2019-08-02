91d01d80297a4bd3c63a9d2be22e9f69cab4eb99
Mashan Rathnayaka <cmashanbr@gmail.com>
2019-08-02 02:27:41 +0000

    Update src/main/java/ae/stock/dao/BankAccountDAO.java

--- src/main/java/ae/stock/dao/BankAccountDAO.java
+++ src/main/java/ae/stock/dao/BankAccountDAO.java
@@ -9,6 +9,50 @@ public class BankAccountDAO {
 	public static List<BankAccount> getAll(){
 		return bank_accounts;
 	}
+	public static BankAccount get(String account_holder) {
+		BankAccount bankAccount=new BankAccount();
+		for(int i=0;i<bank_accounts.size();i++) {
+			if(bank_accounts.get(i).getAccountHolder().equals(account_holder)) {
+				return bank_accounts.get(i);
+			}
+		}
+		return bankAccount;
+	}
+	
+	public static List<BankAccount> withdraw(BankAccount bank_account,double trans_amount) {
+		for(int i=0;i<bank_accounts.size();i++) {
+			if(bank_accounts.get(i).getAccountHolder().equals(bank_account.getAccountHolder())) {
+				bank_account.setAccountBalance(bank_account.getAccountBalance()-trans_amount);
+				bank_accounts.set(i, bank_account);
+				break;
+			}
+		}
+		return bank_accounts;
+	}
+	
+	public static List<BankAccount> deposit(BankAccount bank_account,double trans_amount) {
+		for(int i=0;i<bank_accounts.size();i++) {
+			if(bank_accounts.get(i).getAccountHolder().equals(bank_account.getAccountHolder())) {
+				bank_account.setAccountBalance(bank_account.getAccountBalance()+trans_amount);
+				bank_accounts.set(i, bank_account);
+				break;
+			}
+		}
+		return bank_accounts;
+	}
+	
+	public static List<BankAccount> save(BankAccount bank_account) {
+		boolean found=false;
+		for(BankAccount account:bank_accounts) {
+			if(account.getAccountHolder().equals(bank_account.getAccountHolder())) {
+				found=true;
+			}
+		}
+		if(!found) {
+			bank_accounts.add(bank_account);
+		}
+	    return bank_accounts;
+	}
 	
 
 }
