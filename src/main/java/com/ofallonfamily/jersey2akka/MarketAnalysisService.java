6b621e9c8c1ba54e82b3e6c70e443dcb06a64477
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-08-02 08:31:12 +0000

    Updated Market Analysis Service class

--- src/main/java/com/ofallonfamily/jersey2akka/MarketAnalysisService.java
+++ src/main/java/com/ofallonfamily/jersey2akka/MarketAnalysisService.java
@@ -24,5 +24,34 @@ import scala.concurrent.duration.Duration;
 
 @Path("/analyst")
 public class MarketAnalysisService {
-
+@Context ActorSystem actorSystem;
+    LoggingAdapter log;
+	
+	@GET
+	@Path("stocks")
+    @Produces({ MediaType.APPLICATION_JSON })
+	@ManagedAsync
+    public void getRecommendation(@Suspended final AsyncResponse res) {
+		ActorSelection marketActor = actorSystem.actorSelection("/user/marketRouter");
+    	Timeout timeout = new Timeout(Duration.create(2, "seconds"));
+        Future<Object> future = Patterns.ask(marketActor, new MarketMessage("stock-analysis",""), timeout);
+        future.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable failure, Object result) {
+                if (failure != null) {
+                    if (failure.getMessage() != null) {
+                        HashMap<String,String> response = new HashMap<String,String>();
+                        response.put("error", failure.getMessage());
+                        res.resume(Response.serverError().entity(response).build());
+                    } else {
+                        res.resume(Response.serverError());
+                    }
+                } else {
+                    HashMap<String,Object> response = new HashMap<String,Object>();
+                    response.put("results",result);
+                    res.resume(Response.ok().entity(response).build());
+                }
+            }
+        },actorSystem.dispatcher());
+    }
+	
 }
