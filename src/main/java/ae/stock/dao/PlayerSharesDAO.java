4a176ad9f815dc3066cc803d1ccfe236ced30258
Mashan Rathnayaka <cmashanbr@gmail.com>
2019-08-02 16:41:41 +0000

    Update src/main/java/ae/stock/dao/PlayerSharesDAO.java

--- src/main/java/ae/stock/dao/PlayerSharesDAO.java
+++ src/main/java/ae/stock/dao/PlayerSharesDAO.java
@@ -12,5 +12,14 @@ public class PlayerSharesDAO {
 	public static void save(PlayerShares player_stock) {
 		player_stocks.add(player_stock);	
 	}
+	public static List<PlayerShares> update(PlayerShares player_stock) {
+		for(PlayerShares stock:player_stocks) {
+			if((stock.getPlayer().equals(player_stock.getPlayer())) && (stock.getCompany().equals(player_stock.getCompany()))) {
+				stock.setStock_Count(player_stock.getStock_Count());
+				stock.setStock_Value(player_stock.getStock_Value());
+			}
+		}
+		return player_stocks;
+	}
 
 }
