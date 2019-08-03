d71cda6495c5754183862836fa684dc60d8d2683
Y.G.C.G.Wickramanayake <cgayan.revelation@gmail.com>
2019-08-03 00:52:15 +0000

    Updated MoneyTransaction

--- src/main/java/ae/stock/entities/MoneyTransaction.java
+++ src/main/java/ae/stock/entities/MoneyTransaction.java
@@ -16,6 +16,52 @@ public class MoneyTransaction implements Serializable {
 	private double balance_after;
 	public MoneyTransaction() {
 		
-
+}
+	public MoneyTransaction(String account_holder, String transaction_type, double transaction_amount,
+			double balance_after) {
+		this.account_holder = account_holder;
+		this.transaction_type = transaction_type;
+		this.transaction_amount = transaction_amount;
+		this.balance_after = balance_after;
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();
+		this.trans_time=dtf.format(now);
+	}
+	
+	public String getTime() {
+		return trans_time;
+	}
+	@XmlElement
+	public void setTime(String time) {
+		this.trans_time = time;
+	}
+	public String getAccount_holder() {
+		return account_holder;
+	}
+	@XmlElement
+	public void setAccount_holder(String account_holder) {
+		this.account_holder = account_holder;
+	}
+	public String getTransaction_type() {
+		return transaction_type;
+	}
+	@XmlElement
+	public void setTransaction_type(String transaction_type) {
+		this.transaction_type = transaction_type;
+	}
+	public double getTransaction_amount() {
+		return transaction_amount;
+	}
+	@XmlElement
+	public void setTransaction_amount(double transaction_amount) {
+		this.transaction_amount = transaction_amount;
+	}
+	public double getBalance_after() {
+		return balance_after;
+	}
+	@XmlElement
+	public void setBalance_after(double balance_after) {
+		this.balance_after = balance_after;
+	}
 
 }
