15a06dd43c5ffc8806cdcda1455b6ee415f8f881
Mashan Rathnayaka <cmashanbr@gmail.com>
2019-08-03 03:26:49 +0000

    Update src/main/java/ae/stock/dao/StockTransactionsDAO.java

--- src/main/java/ae/stock/dao/StockTransactionsDAO.java
+++ src/main/java/ae/stock/dao/StockTransactionsDAO.java
@@ -14,6 +14,16 @@ public class StockTransactionsDAO {
 	public static void save(StockTransactions account_transaction) {
 		account_transactions.add(account_transaction);
 	}
+
+	public static List<StockTransactions> getTransactions(String player_name){
+		List<StockTransactions> transactions = new ArrayList<StockTransactions>();
+		for(StockTransactions transaction:account_transactions) {
+			if(transaction.getPlayer().equals(player_name)) {
+				transactions.add(transaction);
+			}
+		}
+		return transactions;
+	}
 	
 
 }
