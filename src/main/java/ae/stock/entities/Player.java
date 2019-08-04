e87729ad3f3cf6fb026296e18af3c1fe69bb2d0b
Y.G.C.G.Wickramanayake <cgayan.revelation@gmail.com>
2019-08-04 05:57:12 +0000

    Updated Player

--- src/main/java/ae/stock/entities/Player.java
+++ src/main/java/ae/stock/entities/Player.java
@@ -14,6 +14,34 @@ public class Player implements Serializable {
 	private double stock_value;
 	private int current_round;
 	private BankAccount bank_account;
-	
+public int getCurrent_round() {
+		return current_round;
+	}
 
+	@XmlElement
+	public void setCurrent_round(int current_round) {
+		this.current_round = current_round;
+	}
+	
+	
+	public BankAccount getBank_account() {
+		return bank_account;
+	}
+	public void setBank_account(BankAccount bank_account) {
+		this.bank_account = bank_account;
+		BankAccountDAO.save(this.bank_account);
+	}
+
+	public Player() {
+		
+	}
+	
+	public Player(String player_name) {
+		this.player_name = player_name;
+		this.stock_value=0;
+		this.current_round=1;
+		this.bank_account=new BankAccount(player_name);
+		BankAccountDAO.save(this.bank_account);
+	}	
+ 
 }
