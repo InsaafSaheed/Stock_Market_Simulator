f4373ec295a6b65aaa4fda3429da3993b3c5b2f8
S H R K de Silva <rajithakds666@gmail.com>
2019-08-04 13:25:43 +0000

    Updated GameTimer

--- src/main/java/ae/stock/core/GameTimer.java
+++ src/main/java/ae/stock/core/GameTimer.java
@@ -8,6 +8,43 @@ import ae.stock.dao.PlayerDAO;
 import ae.stock.dao.PlayerSharesDAO;
 import ae.stock.entities.*;
 public class GameTimer {
-
+static int time;
+    static Timer timer;
+    public static void RoundTimer() {
+    	int delay = 1000;
+        int period = 1000;
+        timer = new Timer();
+        time = 120;
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                setInterval();
+            }
+        }, delay, period);
+    }
+    private static int setInterval() {
+        if (time == 1) {
+            timer.cancel();
+            time=0;
+            if(GameCore.getCurrent_round()<10) {
+            	GameCore.setCurrent_round(GameCore.getCurrent_round()+1);
+            	AiPlayerCore.play();
+            	List<ShareValues> company_trends=GameCore.getCompanyTrends();
+            	for(Company company:CompanyDAO.getAll()) {
+            		for(ShareValues trend:company_trends) {
+            			if(company.getCompany_Name().equals(trend.getCompany_name())){
+            				company.setShare_Vlaue(trend.getRound_values()[GameCore.getCurrent_round()-1]);
+            				CompanyDAO.update(company);
+            				
+            				for(PlayerShares stock:PlayerSharesDAO.getAll()) {
+                    			if(company.getCompany_Name().equals(stock.getCompany())) {
+                    				stock.setStock_Value(stock.getStock_Count()*trend.getRound_values()[GameCore.getCurrent_round()-1]);
+                    				PlayerSharesDAO.update(stock);
+                    			}
+                    		}
+            			}
+            		}
+            		
+            	}
 
 }
