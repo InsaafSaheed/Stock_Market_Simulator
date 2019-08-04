ce0fa6dbc0781a482291403f9bbc1330d2913503
Lahiru samarasinghe <ldilshan782@gmail.com>
2019-08-04 05:54:47 +0000

    Create a class market Evant

--- src/main/java/ae/stock/entities/MarketEvent.java
+++ src/main/java/ae/stock/entities/MarketEvent.java
@@ -1,6 +1,24 @@
 package ae.stock.entities;
 
 public class MarketEvent {
+    private String event_type;
+	private int start_turn;
+	private int end_turn;
+	private int event_value;
+	public String getEvent_type() {
+		return event_type;
+	}
+	public void setEvent_type(String event_type) {
+		this.event_type = event_type;
+	}
+	public int getStart_turn() {
+		return start_turn;
+	}
+	public void setStart_turn(int start_turn) {
+		this.start_turn = start_turn;
+	}
+	public int getEnd_turn() {
+		return end_turn;
 
 	
 }
