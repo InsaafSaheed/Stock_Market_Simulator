bcbe3511d885903cf2bd0439b2682524808d19ca
Lahiru samarasinghe <ldilshan782@gmail.com>
2019-08-04 15:51:11 +0000

    update stock entities package.

--- src/main/java/ae/stock/entities/MarketEvent.java
+++ src/main/java/ae/stock/entities/MarketEvent.java
@@ -1,7 +1,7 @@
 package ae.stock.entities;
 
 public class MarketEvent {
-    private String event_type;
+	private String event_type;
 	private int start_turn;
 	private int end_turn;
 	private int event_value;
@@ -19,6 +19,27 @@ public class MarketEvent {
 	}
 	public int getEnd_turn() {
 		return end_turn;
-
+	}
+	public void setEnd_turn(int end_turn) {
+		this.end_turn = end_turn;
+	}
+	public int getEvent_value() {
+		return event_value;
+	}
+	public void setEvent_value(int event_value) {
+		this.event_value = event_value;
+	}
+	public MarketEvent() {
+		
+	}
+	public MarketEvent(String event_type, int start_turn, int end_turn, int event_value) {
+		this.event_type = event_type;
+		this.start_turn = start_turn;
+		this.end_turn = end_turn;
+		this.event_value = event_value;
+	}
+	
 	
 }
+	
+
