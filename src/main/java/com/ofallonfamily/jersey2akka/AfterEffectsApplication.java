796cb6eb2dfdc82c4c814b936bf70b144dfb266b
Insaaf Saheed <a.insaaf@ucdconnect.ie>
2019-08-04 03:33:51 +0000

    Update src/main/java/com/ofallonfamily/jersey2akka/AfterEffectsApplication.java

--- src/main/java/com/ofallonfamily/jersey2akka/AfterEffectsApplication.java
+++ src/main/java/com/ofallonfamily/jersey2akka/AfterEffectsApplication.java
@@ -18,6 +18,35 @@ import java.util.concurrent.TimeUnit;
 @ApplicationPath("/after-effects")
 public class AfterEffectsApplication extends ResourceConfig {
 
+ private ActorSystem system;
 
+    public AfterEffectsApplication() {
+
+    	GameCore.initialize();
+        system = ActorSystem.create("ExampleSystem");
+        system.actorOf(PlayerActor.mkProps().withRouter(new RoundRobinPool(5)),"playerRouter");
+        system.actorOf(MarketActor.mkProps().withRouter(new RoundRobinPool(5)),"marketRouter");
+        system.actorOf(BrokerActor.mkProps().withRouter(new RoundRobinPool(5)),"brokerRouter");
+        system.actorOf(BankActor.mkProps().withRouter(new RoundRobinPool(5)),"bankRouter");
+        system.actorOf(ClockActor.mkProps().withRouter(new RoundRobinPool(5)),"clockRouter");
+
+        register(new AbstractBinder() {
+            protected void configure() {
+                bind(system).to(ActorSystem.class);
+            }
+        });
+
+        register(new JacksonJsonProvider().
+            configure(SerializationFeature.INDENT_OUTPUT, true));
+
+        packages("com.ofallonfamily.jersey2akka");
+
+    }
+
+    @PreDestroy
+    private void shutdown() {
+        system.shutdown();
+        system.awaitTermination(Duration.create(15, TimeUnit.SECONDS));
+    }
 
 }
